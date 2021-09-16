c0b8f57c5a0a974a29470c534d5dd6ad5a1d1cd9
martinberlin <martin@luckycloud.de>
2021-09-16 11:00:54 +0200

    #45 Start feature adding EspFastSpi class

---
+++ components/CalEPD/include/it8951/epdfastspi.h
@@ -0,0 +1,29 @@
+/* Implement IoInterface for 4 wire SPI communication with 2 CS pins */
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "iointerface.h"
+
+#pragma once
+// Instruction R/W bit set HIGH for data READ
+#define EPD_REGREAD           0x80
+
+class EpdFastSpi : IoInterface
+{
+  public:
+    spi_device_handle_t spi;
+
+    void cmd(const uint8_t cmd) override;
+    void data(uint8_t data) override;
+    // No data is used here (No DC pin)
+    void data(const uint8_t *data, int len) override; 
+    void reset(uint8_t millis) override;
+    void init(uint8_t frequency, bool debug) override;
+
+    void release();
+    uint8_t readTemp();
+    uint8_t readRegister(const uint8_t *data, int len);
+    
+    void waitForBusy();
+  private:
+    bool debug_enabled = true;
+};
